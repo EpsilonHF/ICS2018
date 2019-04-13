bcf258c0a6dbc56b5426f17dabe1cb221e80cd3e
tracer-ics2018 <tracer@njuics.org>
2019-04-13 16:17:11 +0000

    >  compile 171220000 zhf Linux 05af1ff59d5d 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  16:17:11 up 10 days, 14:16,  5 users,  load average: 0.02, 0.05, 0.01 43941b2992ae0f6bfb2c0cb9300fcfe3195e56e8

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -7,37 +7,40 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ,
-  NEQ, AND, OR, MINUS, GE, LE, NUM,  
-  /* TODO: Add more token types */
-
+	TK_NOTYPE = 256, TK_EQ,
+	TK_NEQ, TK_AND, TK_OR, TK_GE, TK_LE, TK_NEG, 
+	TK_DEC, TK_HEX, TK_POINTER, TK_REG,  
 };
 
 static struct rule {
-  char *regex;
-  int token_type;
+	char *regex;
+	int token_type;
+	int priority;
 } rules[] = {
 
   /* TODO: Add more rules.
    * Pay attention to the precedence level of different rules.
    */
-
-  {" +", TK_NOTYPE},    // spaces
-  {"\\+", '+'},         // plus
-  {"==", TK_EQ},        // equal
-  {"!=", NEQ},
-  {"&&", AND},
-  {"\\|\\|", OR},
-  {"-", '-'},
-  {">", '>'},
-  {">=", GE},
-  {"<", '<'},
-  {"<=", LE},
-  {"\\*", '*'},
-  {"/", '/'},
-  {"\\(", '('},
-  {"\\)", ')'},
-  {"[0-9]", NUM},
+	{" +", TK_NOTYPE, 0},			// spaces
+	{"\\t+", TK_NOTYPE, 0},			// spaces
+	{"\\+", '+', 4},				// plus
+	{"==", TK_EQ, 3},				// equal
+	{"!=", TK_NEQ, 3},				// not epual
+    {"&&", TK_AND, 2},				// and
+	{"\\|\\|", TK_OR, 1},			// or
+	{"!", '!', 6},					// not
+	{"-", '-', 4},					// minus
+	{">", '>', 3},					// greater
+	{">=", TK_GE, 3},				// greater or equal
+	{"<", '<', 3},					// less
+	{"<=", TK_LE, 3},				// less or equal
+	{"\\*", '*', 5},				// mul
+	{"/", '/', 5},					// divide
+	{"\\(", '(', 7},				// left bracket
+	{"\\)", ')', 7},				// right bracket
+	{"\\b[0-9]+\\b", TK_DEC, 0},    // decimal number
+	{"\\b0[xX][0-9a-fA-F]+\\b", TK_HEX, 0},   // hex number
+	{"\\$(eax|EAX|ebx|EBX|ecx|ECX|edx|EDX|ebp|EBP|esp|ESP|esi|ESI|edi|EDI|eip|EIP)", TK_REG, 0},  // register
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -48,81 +51,206 @@ static regex_t re[NR_REGEX];
  * Therefore we compile them only once before any usage.
  */
 void init_regex() {
-  int i;
-  char error_msg[128];
-  int ret;
+	int i;
+	char error_msg[128];
+	int ret;
 
-  for (i = 0; i < NR_REGEX; i ++) {
-    ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
-    if (ret != 0) {
-      regerror(ret, &re[i], error_msg, 128);
-      panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
-    }
-  }
+	for (i = 0; i < NR_REGEX; i ++) {
+		ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
+		if (ret != 0) {
+			regerror(ret, &re[i], error_msg, 128);
+			panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
+		}
+	}
 }
 
 typedef struct token {
-  int type;
-  char str[32];
+	int type;
+	char str[32];
+	int priority;
 } Token;
 
 Token tokens[32];
 int nr_token;
 
 static bool make_token(char *e) {
-  int position = 0;
-  int i;
-  regmatch_t pmatch;
+	int position = 0;
+	int i;
+	regmatch_t pmatch;
 
-  nr_token = 0;
+	nr_token = 0;
 
-  while (e[position] != '\0') {
+	while (e[position] != '\0') {
     /* Try all rules one by one. */
-    for (i = 0; i < NR_REGEX; i ++) {
-      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-        char *substr_start = e + position;
-        int substr_len = pmatch.rm_eo;
+		for (i = 0; i < NR_REGEX; i ++) {
+			if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+				char *substr_start = e + position;
+				int substr_len = pmatch.rm_eo;
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
-        position += substr_len;
+				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+					i, rules[i].regex, position, substr_len, substr_len, substr_start);
+				position += substr_len;
 
-        /* TODO: Now a new token is recognized with rules[i]. Add codes
-         * to record the token in the array `tokens'. For certain types
-         * of tokens, some extra actions should be performed.
-         */
+				/* TODO: Now a new token is recognized with rules[i]. Add codes
+				 * to record the token in the array `tokens'. For certain types
+                 * of tokens, some extra actions should be performed.
+				 */
 
-        switch (rules[i].token_type) {
-			case TK_NOTYPE: break;
-			case NUM: token[nr_token].type = rules[i].token_type;
-				      strncpy(token[nr_token].str, substr_start, substr_len);
-					  token[nr_token].str[substr_len] = '\0';	  
-			default: token[nr_token].type = rules[i].token_type;
-					 nr_token++;
-					 break;
-        }
-		position += substr_len;
-        break;
-      }
-    }
+				switch (rules[i].token_type) {
+					case TK_NOTYPE: 
+						break;
 
-    if (i == NR_REGEX) {
-      printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
-      return false;
-    }
-  }
+					case TK_REG:
+						tokens[nr_token].type = rules[i].token_type;
+						strncpy(tokens[nr_token].str, substr_start+1, substr_len-1);
+						tokens[nr_token].str[substr_len-1] = '\0';
+						tokens[nr_token].priority = rules[i].priority;
+						nr_token++;
+						break;
 
-  return true;
+					case TK_DEC: 
+					case TK_HEX:			 
+						tokens[nr_token].type = rules[i].token_type;
+						strncpy(tokens[nr_token].str, substr_start, substr_len);
+						tokens[nr_token].str[substr_len] = '\0';
+						tokens[nr_token].priority = rules[i].priority;
+						nr_token++;
+						break;
+
+					default: 
+						tokens[nr_token].type = rules[i].token_type;
+						tokens[nr_token].priority = rules[i].priority;
+						nr_token++;
+						break;
+				}
+				position += substr_len;
+				break;
+			}
+		}
+
+		if (i == NR_REGEX) {
+			printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
+			return false;
+		}
+	}
+
+	return true;
+}
+
+/* check the brackets is right */  
+static bool check_parentheses(int l, int r) {
+
+	if (tokens[l].type == '(' && tokens[r].type == ')') {
+		int l_count = 0, r_count = 0;
+		for (int i = l + 1; i < r; i++) {
+			if (tokens[i].type == '(') l_count++;
+			if (tokens[i].type == ')') r_count++;
+			if (r_count > l_count) return false;
+		}
+		
+		return l_count == r_count;
+	}
+
+	return false;
+}
+
+// load data in register
+static uint32_t read_reg(char *addr) {
+	return 0;
+}
+
+// find dominant operator in experssion
+static int dominant(int l, int r) {
+	
+	int op = l, pri = 8, count = 0;
+	for (int i = l; i <= r; i++) {
+		if (tokens[i].type == '(') 
+			count++;
+		else if (tokens[i].type == ')') 
+			count--;
+		if (count > 0)
+			continue;
+		if (tokens[i].type == '+' || tokens[i].type == '-' || \
+			tokens[i].type == '*' || tokens[i].type == '/') {
+			if (tokens[i].priority <= pri) {
+				op = i;
+				pri = tokens[i].priority;
+			}
+		}
+	}
+
+	return op;
+}
+
+static uint32_t eval(int l, int r, bool *success) {
+	if (!*success) {
+		return -1;
+	}
+
+	if (l > r) {
+		Assert(l > r, "Wrong expression\n");
+		return -1;
+	}
+
+	if (l == r) {
+		int num = 0;
+		switch (tokens[l].type) {
+			case TK_DEC: 
+				sscanf(tokens[l].str, "%d", &num);
+				return num;
+			case TK_HEX:
+				sscanf(tokens[l].str, "%x", &num);
+				return num;
+			case TK_REG:
+				return read_reg(tokens[l].str);
+		}
+	}
+
+	else if (check_parentheses(l, r) == true) {
+		return eval(l - 1, r + 1, success);
+	}
+
+	else {
+		int op = dominant(l, r);
+		//if (token[i].type == TK_NEG)
+		//	return -eval(l + 1, r, *success);
+		int val1 = eval(l, op - 1, success);
+		int val2 = eval(op + 1, r, success);
+		
+		switch(tokens[op].type) {
+			case '+' : 
+				return val1 + val2;
+			case '-' :
+				return val1 - val2;
+			case '*' :
+				return val1 * val2;
+			case '/' :
+				return val1 / val2;
+			default:
+				*success = false;
+				return -1;
+		}
+	}
+
+	return 0;
 }
 
 uint32_t expr(char *e, bool *success) {
-  if (!make_token(e)) {
-    *success = false;
-    return 0;
-  }
+	if (!make_token(e)) {
+		*success = false;
+		return 0;
+	}
 
-  /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+	/* check poniter and negative number */
+	for (int i = 0; i < nr_token; i++) {
 
-  return 0;
+		if (tokens[i].type == '-' && (i == 0 || tokens[i-1].type == '+' || \
+			tokens[i-1].type == '(' || tokens[i-1].type == '-')) {
+			tokens[i].type = TK_NEG;
+			tokens[i].priority = 6;
+		}
+	}
+
+	*success = true;
+	return eval(0 , nr_token - 1, success);
 }
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -7,9 +7,76 @@
 
 // this should be enough
 static char buf[65536];
-static inline void gen_rand_expr() {
-  buf[0] = '\0';
+static int state = 0;
+
+static inline int choose(int n) {
+	return rand() % n;
 }
+
+
+static inline void gen_num() {
+	uint32_t num = (uint32_t)rand() % 100 + 1;
+	switch (choose(2)) {
+		case 0:
+			sprintf(buf + strlen(buf), "%u", num);
+			break;
+		case 1:
+			sprintf(buf + strlen(buf), "%#x", num);
+			break;
+		default:
+			break;
+	}
+}
+
+
+static inline void gen(char c) {
+	size_t i = strlen(buf);
+	if (i > 65536) {
+		state = 1;
+		return;
+	}
+	buf[i] = ' ';
+	buf[i+1] = c;
+	buf[i+2] = '\0';
+}
+
+
+static inline void gen_rand_op() {
+	switch (choose(4)) {
+		case 0:
+			gen('+');
+			break;
+		case 1:
+			gen('-');
+			break;
+		case 2:
+			gen('*');
+			break;
+		case 3:
+			gen('/');
+			break;
+	}
+}
+
+
+static inline void gen_rand_expr() {
+	switch (choose(3)) {
+		case 0:
+			gen_num();
+			break;
+		case 1:
+			gen('(');
+			gen_rand_expr();
+			gen(')');
+			break;
+		case 2:
+			gen_rand_expr();
+			gen_rand_op();
+			gen_rand_expr();
+			break;
+	}	
+} 
+
 
 static char code_buf[65536];
 static char *code_format =
@@ -21,34 +88,40 @@ static char *code_format =
 "}";
 
 int main(int argc, char *argv[]) {
-  int seed = time(0);
-  srand(seed);
-  int loop = 1;
-  if (argc > 1) {
-    sscanf(argv[1], "%d", &loop);
-  }
-  int i;
-  for (i = 0; i < loop; i ++) {
-    gen_rand_expr();
+	int seed = time(0);
+	srand(seed);
+	int loop = 1;
+	if (argc > 1) {
+		sscanf(argv[1], "%d", &loop);
+	}
+	int i;
+	for (i = 0; i < loop; i ++) {
 
-    sprintf(code_buf, code_format, buf);
+		do {
+			memset(buf, 0, 65536);
+			state = 0;
+			gen_rand_expr();
+		} 
+		while (state);
 
-    FILE *fp = fopen(".code.c", "w");
-    assert(fp != NULL);
-    fputs(code_buf, fp);
-    fclose(fp);
+		sprintf(code_buf, code_format, buf);
 
-    int ret = system("gcc .code.c -o .expr");
-    if (ret != 0) continue;
+		FILE *fp = fopen(".code.c", "w");
+		assert(fp != NULL);
+		fputs(code_buf, fp);
+		fclose(fp);
 
-    fp = popen("./.expr", "r");
-    assert(fp != NULL);
+		int ret = system("gcc .code.c -o .expr");
+		if (ret != 0) continue;
 
-    int result;
-    fscanf(fp, "%d", &result);
-    pclose(fp);
+		fp = popen("./.expr", "r");
+		assert(fp != NULL);
 
-    printf("%u %s\n", result, buf);
-  }
-  return 0;
+		int result;
+		fscanf(fp, "%d", &result);
+		pclose(fp);
+
+		printf("%u %s\n", result, buf);
+	}
+	return 0;
 }
