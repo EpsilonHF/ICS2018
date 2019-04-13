087959bf9cc3c51cb504e108d2f069df5522b536
tracer-ics2018 <tracer@njuics.org>
2019-04-13 22:03:12 +0000

    >  compile 171220000 zhf Linux 05af1ff59d5d 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  22:03:12 up 10 days, 15:08,  5 users,  load average: 0.01, 0.02, 0.00 e7b6f4baf1bf5dacde1408b82e354152164db9e1

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,12 +1,30 @@
+#include <stdio.h>
+#include "../include/monitor/expr.h"
+
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
 int main(int argc, char *argv[]) {
-  /* Initialize the monitor. */
-  int is_batch_mode = init_monitor(argc, argv);
+	/* Initialize the monitor. */
+	int is_batch_mode = init_monitor(argc, argv);
 
+	bool success = true;
+	uint32_t result;
+	char buf[65536];
+	while (scanf("%u %s", &result, buf) != EOF) {
+		bool equ = false;
+		uint32_t res = expr(buf, &success);
+		equ = res == result;
+		if (success) {
+			if (!equ) printf("%d %u %u %s\n", equ, res, result, buf);
+		} 
+		else {
+			printf("%u %s\n", result, buf);
+			printf("P Error\n");
+		}
+	}
   /* Receive commands from user. */
-  ui_mainloop(is_batch_mode);
+	ui_mainloop(is_batch_mode);
 
-  return 0;
+	return 0;
 }
