7a79fbda1f4d958a26a7d910458947ebdcc5d6c1
tracer-ics2018 <tracer@njuics.org>
2019-04-14 12:03:58 +0000

    >  compile 171220000 zhf Linux 05af1ff59d5d 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  12:03:58 up 11 days,  2:18,  5 users,  load average: 0.24, 0.08, 0.02 95e45eb064f77feab7d7897dba5ce7fad140c78

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -4,12 +4,22 @@
 #include "common.h"
 
 typedef struct watchpoint {
-  int NO;
-  struct watchpoint *next;
-
-  /* TODO: Add more members if necessary */
-
-
+	int NO;
+	struct watchpoint *next;
+	char exp[256];
+	uint32_t val;
 } WP;
+
+WP* new_wp();
+
+int free_wp(WP*);
+
+WP* get_head();
+
+WP* get_free();
+
+int check_watchpoints();
+
+int print_watchpoints();
 
 #endif
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -7,17 +7,99 @@ static WP wp_pool[NR_WP];
 static WP *head, *free_;
 
 void init_wp_pool() {
-  int i;
-  for (i = 0; i < NR_WP; i ++) {
-    wp_pool[i].NO = i;
-    wp_pool[i].next = &wp_pool[i + 1];
-  }
-  wp_pool[NR_WP - 1].next = NULL;
+	int i;
+	for (i = 0; i < NR_WP; i ++) {
+		wp_pool[i].NO = i;
+		wp_pool[i].next = &wp_pool[i + 1];
+	}
+	wp_pool[NR_WP - 1].next = NULL;
 
-  head = NULL;
-  free_ = wp_pool;
+	head = NULL;
+	free_ = wp_pool;
 }
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP* new_wp() {
+	if (free_ == NULL) {
+		printf("No Free WatchPointer!\n");
+		return NULL;
+	}
+	WP *ptr = free_;
+	free_ = free_->next;
 
+	ptr->next = head;
+	head = ptr;
+
+	return head;
+}
+
+
+int free_wp(WP *wp) {
+	if (wp == NULL) {
+		printf("Can't free NULL Pointer!\n");
+		return 1;
+	}
+
+	if (wp == head) {
+		head = wp->next;
+		wp->next = free_;
+		free_ = wp;
+		return 0;
+	}
+
+	for (WP *ptr = head; ptr != NULL; ptr = ptr->next) {
+		if (ptr->next == wp) {
+			ptr->next = ptr->next->next;
+			wp->next = free_;
+			free_ = wp;
+			return 0;
+		}	
+	}
+
+	return 1;
+}
+
+
+WP* get_head() {
+	return head;
+}
+
+
+WP* get_free() {
+	return free_;
+}
+
+int check_watchpoints() {
+	WP *ptr = head;
+	bool success;
+	int state = 0;
+	uint32_t res;
+	while (ptr) {
+		res = expr(ptr->exp, &success);
+		if (res != ptr->val) {
+			printf("NO: %d EXPR: %s Old: %#x NEW: %#x\n", ptr->NO, \
+				   ptr->exp, ptr->val, res); 	
+			ptr->val = res;
+			state = 1;
+		}
+		ptr = ptr->next;
+	}
+	return state;
+}
+
+
+int print_watchpoints(WP *ptr) {
+	if (ptr == NULL) {
+		printf("No watchpoint!\n");
+		return 1;
+	}
+	printf("NO\tEXPR\t\tValue\n");
+
+	while(ptr != NULL) {
+		printf("%-d\t%-16s%#08x  %d\n",ptr->NO, ptr->exp, ptr->val, ptr->val);
+		ptr = ptr->next;
+	}
+
+	return 0;
+}
